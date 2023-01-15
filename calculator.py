dc6aad65170d454bdfd2c3255461a885f24656b9
tinyfro <adamrzu@gmail.com>
2023-01-15 19:21:20 +1100

    Basic implementation

--- calculator.py
+++ calculator.py
@@ -6,6 +6,32 @@
 __author__      = "tinyfro"
 __copyright__   = "Copyright 2023, tinyfro"
 
+
+import os
+
 def main():
+    pass
+
+def clear_cns():
+    """Checks for the operating system and clears all previous entries and displays from the console."""
+    return os.system("cls" if os.name == "nt" else "clear")
+
+def input_mgmt():
+    pass
+
+def user_input():
+    pass
+
+def addition():
+    pass
+
+def subtraction():
+    pass
+
+def multiplication():
+    pass
+
+def division():
+    pass
 
 main()
