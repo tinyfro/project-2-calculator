7ef73f54e08866543182abd44fe860e73e36af37
Adam <32489045+tinyfro@users.noreply.github.com>
2023-01-15 18:45:17 +1100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""calculator.py: Lightweight calculator application (still deciding if GUI or Console based). Made for educational and fun purposes."""
+
+__author__      = "tinyfro"
+__copyright__   = "Copyright 2023, tinyfro"
+
+def main():
+
+main()
